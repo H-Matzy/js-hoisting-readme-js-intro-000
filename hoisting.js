2da342717f3c4e71a986110c8c9cfc3bb066441d
positive-proc-6135 <haydenmatz97@gmail.com>
2020-09-23 19:40:36 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -13,10 +13,10 @@ function crazy() {
   // fix the code in here:
   var thisIsCrazy = function (){
     console.log("hey!!!")
-  thisIsCrazy();
-
-
   }
+thisIsCrazy();
+
+
 }
 
 function sayMyName() {
